074110552d0c82d5ed388048d1386c1f7958430a
nookiehi <jamesnookie2002@gmail.com>
2022-12-16 02:54:29 +0800

    Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,19 +1,43 @@
 package LeapYear;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.GregorianCalendar;
 
-public class LeapYearGUI extends JFrame {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,checkLeapYear());
+            }
+        });
+    }
+
+    public String checkLeapYear() {
+        try {
+            GregorianCalendar calendar = new GregorianCalendar();
+            int year = Integer.parseInt(tfYear.getText());
+            if (calendar.isLeapYear(year)) {
+                return "Leap Year";
+            }
+            return "Not a Leap Year";
+        } catch (Exception e) {
+            return "Invalid input. Not an integer";
+        }
+    }
 
     public static void main(String[] args){
         LeapYearGUI gui = new LeapYearGUI();
         gui.setContentPane(gui.panel1);
         gui.setSize(200,300);
+        gui.setTitle("Leap Year Checker");
         gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         gui.setVisible(true);
-
     }
 }
