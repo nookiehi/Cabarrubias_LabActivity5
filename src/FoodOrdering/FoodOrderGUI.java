9e56775870cf9ac6089dedec86b3e9b53c451e0f
nookiehi <jamesnookie2002@gmail.com>
2022-12-16 23:45:20 +0800

    Rename FoodOrdering components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -4,7 +4,15 @@ import javax.swing.*;
 
 public class FoodOrderGUI {
     private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
     private JButton orderButton;
+    private JCheckBox cBurger;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cSundae;
+    private JCheckBox cTea;
+    private JCheckBox cFries;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
 }
