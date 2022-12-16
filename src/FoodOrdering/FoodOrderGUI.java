3b5e126db755bf7d0e513ccaade664121b41b990
nookiehi <jamesnookie2002@gmail.com>
2022-12-17 00:49:21 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,12 +1,14 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
-    private JButton orderButton;
+    private JButton btnOrder;
     private JCheckBox cBurger;
     private JCheckBox cSoftDrinks;
     private JCheckBox cSundae;
@@ -15,6 +17,47 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String price = String.format("%.2f",price());
+                JOptionPane.showMessageDialog(panel1,"The total price is Php " + price);
+            }
+        });
+    }
+    public double price(){
+        double price = 0;
+        if(cPizza.isSelected()){
+            price += 100;
+        }
+        if(cBurger.isSelected()){
+            price += 80;
+        }
+        if(cFries.isSelected()){
+            price += 65;
+        }
+        if(cSoftDrinks.isSelected()){
+            price += 55;
+        }
+        if(cTea.isSelected()){
+            price += 50;
+        }
+        if(cSundae.isSelected()){
+            price += 40;
+        }
+        if(rbNone.isSelected()){
+            price -= 0;
+        }else if(rb5.isSelected()){
+            price -= 0.05 * price;
+        }else if(rb10.isSelected()){
+            price -= 0.10 * price;
+        }else if(rb15.isSelected()){
+            price -= 0.15 * price;
+        }
+        return price;
+    }
 
     public static void main(String[] args) {
         FoodOrderGUI order = new FoodOrderGUI();
